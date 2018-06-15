8ea9a6e2e255c7ea4d9071b4f878f029bb3c891b
张进 <zhangjin2233@gmail.com>
2018-06-15 16:41:48 +0800

    能够运行的基础版本

---
+++ src/main/java/com/ppxia/billing/authortymag/service/impl/AuthorityHandleServiceImpl.java
@@ -0,0 +1,29 @@
+package com.ppxia.billing.authortymag.service.impl;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.ppxia.billing.authortymag.dao.IAuthorityHandleDao;
+import com.ppxia.billing.authortymag.service.IAuthoryHandleService;
+import com.ppxia.billing.beans.AuthorityBean;
+
+@Service
+public class AuthorityHandleServiceImpl implements IAuthoryHandleService {
+
+	@Resource
+	private IAuthorityHandleDao authorityHandleDaoImpl;
+	
+	@Override
+	public void deleteAuthority(Long id) {
+		// TODO Auto-generated method stub
+		authorityHandleDaoImpl.delete(id);
+	}
+
+	@Override
+	public void saveAuthority(AuthorityBean authory) {
+		// TODO Auto-generated method stub
+		authorityHandleDaoImpl.saveAndFlush(authory);
+	}
+
+}
